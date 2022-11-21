a600eaa4d2b3ffedad039cf5f9a49be1e6cfb7b6
Paavveel <pa.tomilin@gmail.com>
2022-11-21 16:37:03 +0300

    feat: add allowedOrigins for cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://technotes-mcbl.onrender.com',
+];
 
 module.exports = allowedOrigins;
